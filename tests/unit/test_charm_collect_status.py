ee76c24305ff9ad3347dd65bca1dae18d870b5de
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-12-09 09:25:00 -0500

    feat: add log-level configuration option (#380)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- tests/unit/test_charm_collect_status.py
+++ tests/unit/test_charm_collect_status.py
@@ -22,6 +22,22 @@ class TestCharmCollectStatus(UDRUnitTestFixtures):
 
         assert state_out.unit_status == BlockedStatus("Scaling is not implemented for this charm")
 
+    def test_given_invalid_log_level_config_when_collect_unit_status_then_status_is_blocked(
+        self,
+    ):
+        container = testing.Container(name="udr", can_connect=True)
+        state_in = testing.State(
+            leader=True,
+            config={"log-level": "invalid"},
+            containers={container},
+        )
+
+        state_out = self.ctx.run(self.ctx.on.collect_unit_status(), state_in)
+
+        assert state_out.unit_status == BlockedStatus(
+            "The following configurations are not valid: ['log-level']"
+        )
+
     def test_given_relations_not_created_when_collect_unit_status_then_status_is_blocked(self):
         container = testing.Container(
             name="udr",
